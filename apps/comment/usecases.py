45ccf31c9ca268ba7ed76b36c7e698d91a3d3a9a
pramish karkee <pramishkarkee@pramishs-Air.mshome.net>
2022-08-12 21:09:37 +0545

    fix

--- apps/comment/usecases.py
+++ apps/comment/usecases.py
@@ -55,6 +55,7 @@ class ListChildApplicationComment(BaseUseCase):
         self._factory()
         return self._child_comment
     def _factory(self):
+        print(":**************")
         self._child_comment = ApplicationComments.objects.filter(
             parent_comment=self._comment,
         ).order_by('created_at')
